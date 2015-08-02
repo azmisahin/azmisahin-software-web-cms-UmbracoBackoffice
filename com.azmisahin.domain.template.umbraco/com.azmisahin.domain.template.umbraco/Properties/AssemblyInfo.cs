0acc3149029982a97a6f000d552c939bdae1b51f
Azmi ŞAHİN <azmisahin@outlook.com>
2015-08-02 14:44:16 +0300

    Nuget Package Version Update

--- com.azmisahin.domain.template.umbraco/com.azmisahin.domain.template.umbraco/Properties/AssemblyInfo.cs
+++ com.azmisahin.domain.template.umbraco/com.azmisahin.domain.template.umbraco/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("aea1ca16-eeea-4d6c-aff8-7fada35e0022")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
